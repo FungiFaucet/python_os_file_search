8ff002abe5775ed03c432426a614505589ae6980
FungiFaucet <123754382+FungiFaucet@users.noreply.github.com>
2023-04-19 11:09:51 -0400

    initial commit
    this python script looks through your OS at the specified directory and searches for files over a set size limt

---
+++ os_file_finder_functions.py
@@ -0,0 +1,26 @@
+import os
+import time
+from tqdm import tqdm
+
+def find_by_file_size(file_size, directory):
+    files_over_file_size = 0
+    files_under_file_size = 0
+    bytes_in_gig = 1073741824
+    file_size = file_size * bytes_in_gig # converting gigabytes to bytes for filesize comparison
+    total_big_files_size_combined = 0
+
+    for dir_path, dir_names, filenames in tqdm(os.walk(directory)):
+        for filename in filenames:
+            filepath = os.path.join(dir_path, filename)
+            size = os.path.getsize(filepath)
+            if size >= file_size:
+                print(f'{filename} : {size / bytes_in_gig:.2f} Gigabytes ')
+                files_over_file_size += 1
+                total_big_files_size_combined += size
+            else:
+                files_under_file_size += 1
+
+    total_files = files_over_file_size + files_under_file_size
+    print(f'Total files checked: {total_files}')
+    print(f'Total files over size limit: {files_over_file_size}')
+    print(f'Total storage saved if 100% of files over size limit are deleted: {total_big_files_size_combined / bytes_in_gig:.2f} Gigabytes')
---
+++ os_file_finder_main.py
@@ -0,0 +1,17 @@
+from os_file_finder_functions import *
+import os_file_finder_functions as ff
+
+
+#######################################
+############ VARIABLES ################
+
+file_size = .5 # in gb
+directory = 'K:\!!!!Movies'
+
+#######################################
+
+
+
+# Takes two arguments (filesize, directory)
+ff.find_by_file_size(file_size, directory)
+
