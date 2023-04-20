599b330ba526a976fb13f6be7a3d341946abe6dc
FungiFaucet <123754382+FungiFaucet@users.noreply.github.com>
2023-04-19 22:14:10 -0400

    none

--- os_file_finder_functions.py
+++ os_file_finder_functions.py
@@ -8,13 +8,15 @@ def find_by_file_size(file_size, directory):
     bytes_in_gig = 1073741824
     file_size = file_size * bytes_in_gig # converting gigabytes to bytes for filesize comparison
     total_big_files_size_combined = 0
+    found_files_over_size = []
 
-    for dir_path, dir_names, filenames in tqdm(os.walk(directory)):
+    for dir_path, dir_names, filenames in os.walk(directory):
         for filename in filenames:
             filepath = os.path.join(dir_path, filename)
             size = os.path.getsize(filepath)
             if size >= file_size:
-                print(f'{filename} : {size / bytes_in_gig:.2f} Gigabytes ')
+                #print(f'{filename} : {size / bytes_in_gig:.2f} Gigabytes ')
+                found_files_over_size
                 files_over_file_size += 1
                 total_big_files_size_combined += size
             else:
